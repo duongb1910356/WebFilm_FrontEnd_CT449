a7754594a734df03f4d327d518b1370ed476e9d4
duongb1910356 <duongb1910356@student.ctu.edu.vn>
2023-06-02 19:57:53 +0700

    them giao dien admin

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import { createWebHistory, createRouter } from "vue-router";
 import HomePage from "@/views/HomePage.vue";
 import WatchPage from "@/views/WatchPage.vue";
 import DetailPage from "@/views/DetailPage.vue";
+import AdminPage from "@/views/AdminPage.vue";
+
 const routes = [
     {
         path: "/",
@@ -20,7 +22,13 @@ const routes = [
         name: "watchpage",
         component: WatchPage,
         props: true,
-    }
+    },
+
+    {
+        path: "/admin",
+        name: "admin",
+        component: AdminPage,
+    },
 ];
 
 const router = createRouter({ history: createWebHistory(import.meta.env.BASE_URL), routes, });
---
+++ src/views/AdminPage.vue
@@ -0,0 +1,34 @@
+<template>
+    <h1>Thiết kế temple ở đây nghe Huy ùi</h1>
+</template>
+
+<script>
+import { useFilmStore } from "../stores/films";
+import { mapStores } from "pinia";
+import FilmList from "@/components/FilmList.vue";
+import Paginate from "@/components/Paginate.vue";
+import { usePageStore } from "../stores/page.store";
+
+export default {
+    components: {
+        FilmList, Paginate,
+    },
+    data() {
+        return {
+           
+        }
+    },
+    computed: {
+       
+    },
+    methods: {
+        
+    },
+    created() {
+    },
+    mounted() {
+    }
+}
+</script>
+
+<style></style>
